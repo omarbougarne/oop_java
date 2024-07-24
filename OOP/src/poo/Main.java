cbef15f752ccff703cb4fbaba20881dec5729141
omarbougarne <omarwimu@gmail.com>
2024-07-24 17:08:49 +0100

    oop java

---
+++ OOP/src/poo/Main.java
@@ -0,0 +1,30 @@
+package poo;
+
+
+public class Main {
+
+
+
+	public static void main(String[] args) {
+
+		
+
+		Car myCar1 = new Car();
+
+		Car myCar2 = new Car();
+
+
+
+		System.out.println(myCar1.make);
+
+		System.out.println(myCar2.model);
+
+
+
+		myCar1.drive();
+
+		myCar1.brake();
+
+	}
+
+}
